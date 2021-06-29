9edaffaa1fe93ca1c9b11299e9384df839082db3
KimMyeongSeok <audxo112@naver.com>
2021-06-29 16:30:13 +0900

    add static root

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -223,6 +223,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "static/"
+
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
