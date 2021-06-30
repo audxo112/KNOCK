9a083a91d6c4369f54b8bef442ef34ee389fed8d
KimMyeongSeok <audxo112@naver.com>
2021-06-30 13:37:13 +0900

    urls change

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,35 +17,12 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
-from api.views import VerifyJWTAPIView, RefreshJWTAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("token/verify/", VerifyJWTAPIView.as_view()),
-    path("token/refresh/", RefreshJWTAPIView.as_view()),
     path(
-        "users/",
-        include("users.urls", namespace="users"),
-    ),
-    path(
-        "themes/",
-        include("themes.urls", namespace="themes"),
-    ),
-    path(
-        "curations/",
-        include("curations.urls", namespace="curations"),
-    ),
-    path(
-        "frames/",
-        include("frames.urls", namespace="frames"),
-    ),
-    path(
-        "lists/",
-        include("lists.urls", namespace="lists"),
-    ),
-    path(
-        "logs/",
-        include("knocklogs.urls", namespace="knocklogs"),
+        "api/",
+        include("api.urls", namespace="api"),
     ),
 ]
 
